f3887cf346f310a70eda1e6c02961d23e7b01275
Karaokesnr <ann164628@gmail.com>
2022-01-26 16:15:25 +0300

    Constants notes 26th jan

---
+++ constants.py
@@ -0,0 +1,12 @@
+#Constants
+#strings
+
+name = input ("Enter your name: ")
+print (f'Your name is {name}')
+
+#numeric
+age = int (input ("Enter your age: "))#by default the input function returns a string, thuss type conversion is needed
+print (f'Your age is {age}')
+
+discount = float (input("Enter a discount: "))
+print (f'total discount = ksh{discount}')
