25622925eab932c1b55bd72d81f25e8ea787b363
Karaokesnr <ann164628@gmail.com>
2022-01-26 16:09:34 +0300

    Variables class notes 26th jan

---
+++ Variables.py
@@ -0,0 +1,11 @@
+#variables
+#variables are case sensitive 
+#variables can only contain alphanumerics ie no.s and alphabets
+
+x = 2 #Assignment statement
+x = x + 2 #Assignment with expression
+print (x) #print statement
+
+x = int (input("Enter the value of x: "))
+x +=2
+print (f'updated value of x = {x}')
